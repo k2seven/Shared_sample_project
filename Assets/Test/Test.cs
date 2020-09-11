ba4b732c88736b1e83cecb08bdaf0c57dbb8f011
liyiyuan@ihuman.com <liyiyuan@ihuman.com>
2020-09-11 11:40:53 +0800

    add xlua

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LuaEnv env = new LuaEnv();
+		env.DoString(@"
+			print('lua start')
+			
+			GameObject = CS.UnityEngine.GameObject
+			Vector3 = CS.UnityEngine.Vector3
+
+			local go = GameObject('test_go')
+			go.transform.localScale = Vector3.one	
+			go.name = 'lua_obj'
+		");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
